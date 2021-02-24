b086b13d2e77a7b0c0c05fe3f434a21bac0fe873
XuyingSwift <xuyingswift@gmail.com>
2021-02-24 16:53:36 -0700

    more charts

--- src/main/java/HistoryDataParser.java
+++ src/main/java/HistoryDataParser.java
@@ -1,15 +1,19 @@
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.gson.JsonArray;
+import org.json.simple.JSONArray;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryDataParser {
     private static final String VALUE = "value";
+    private static final String DATE = "date";
 
     public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
@@ -26,14 +30,29 @@ public class HistoryDataParser {
         ObjectNode security4 = NodeHelper.getSecurity(file4, mapper);
         ObjectNode security5 = NodeHelper.getSecurity(file5, mapper);
 
-        root.put("date", "value");
-        root.put("2015", security1.get(VALUE));
-        root.put("2016", security2.get(VALUE));
-        root.put("2017", security3.get(VALUE));
-        root.put("2018", security4.get(VALUE));
-        root.put("2019", security5.get(VALUE));
+        security1.put(DATE, "1-jan-21");
+        security1.put(VALUE, security1.get(VALUE));
 
-        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+        security2.put(DATE, "1-feb-21");
+        security2.put(VALUE, security2.get(VALUE));
+
+        security3.put(DATE, "1-mar-21");
+        security3.put(VALUE, security3.get(VALUE));
+
+        security4.put(DATE, "1-apr-21");
+        security4.put(VALUE, security4.get(VALUE));
+
+        security5.put(DATE, "1-may-21");
+        security5.put(VALUE, security5.get(VALUE));
+
+        List<ObjectNode> list = new ArrayList<>();
+        list.add(security1);
+        list.add(security2);
+        list.add(security3);
+        list.add(security4);
+        list.add(security5);
+
+        String json = JSONArray.toJSONString(list);
 
         try{
             FileWriter writer = new FileWriter("historyData.json");
